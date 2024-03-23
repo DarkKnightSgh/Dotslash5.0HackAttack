39898b573513ba29e0fd05e34e6126e5bf58a425
Siri Gowri H <sirigowriharish@outlook.com>
2024-03-23 16:09:10 +0530

    Addition

--- preprocess_caption.py
+++ preprocess_caption.py
@@ -1,37 +1,26 @@
-
 from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
 import torch
+from PIL import Image
+import tkinter as tk
+from tkinter import filedialog
 
-# Load model and tokenizer
-print("Loading the model")
 model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
+
 max_length = 16
 num_beams = 4
 gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
 
-# Function to generate caption
-def generate_caption(image_path):
-    image = Image.open(image_path)
-    image = image.convert("RGB")
-    pixel_values = feature_extractor(images=[image], return_tensors="pt").pixel_values.to(device)
-    output_ids = model.generate(pixel_values, **gen_kwargs)
-    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-    caption = preds[0].strip()
-    return caption
-
-def generate_caption(path):
-    images = []
-    for image_path in path:
-        i_image = Image.open(image_path)
-        if i_image.mode != "RGB":
-            i_image = i_image.convert(mode="RGB")
-        images.append(i_image)
+def predict_caption(image_path):
+    i_image = Image.open(image_path)
+    if i_image.mode != "RGB":
+        i_image = i_image.convert(mode="RGB")
     
-    pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
+    pixel_values = feature_extractor(images=i_image, return_tensors="pt").pixel_values
     pixel_values = pixel_values.to(device)
     output_ids = model.generate(pixel_values, **gen_kwargs)
 
@@ -40,3 +29,14 @@ def generate_caption(path):
     print("Final Caption is:", preds)
     return preds
 
+def select_image():
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
+    file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.jpg *.jpeg *.png")])
+    if file_path:
+        caption = predict_caption(file_path)
+        return caption
+    else:
+        return None
+
+caption = select_image()
