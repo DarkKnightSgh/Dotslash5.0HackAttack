9e91c6f0b144ca33d1c4efef69d28f77f0e456b3
Siri Gowri H <sirigowriharish@outlook.com>
2024-03-23 18:47:38 +0530

    Backend

--- Backend/app.py
+++ Backend/app.py
@@ -1,9 +1,16 @@
 from flask import Flask, request, jsonify, render_template
 from preprocess_caption import predict_caption
 import os
+import webbrowser
 
+# Create Flask application instance
 app = Flask(__name__)
 
+# Define the path to the templates directory
+template_dir = os.path.abspath('../templates')
+app = Flask(__name__, template_folder=template_dir)
+
+# Define routes
 @app.route('/')
 def index():
     return render_template('mock.html')
@@ -37,5 +44,8 @@ def upload_file():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+# Run the Flask application
 if __name__ == '__main__':
+    # Open the default web browser and navigate to the appropriate URL
+    webbrowser.open('http://127.0.0.1:5000/')
     app.run(debug=True)
