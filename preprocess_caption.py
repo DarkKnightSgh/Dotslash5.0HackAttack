558f1f77a7ba14ea570242d5189c21294eebf546
Siri Gowri H <106529095+DarkKnightSgh@users.noreply.github.com>
2024-03-23 15:30:34 +0530

    Add files via upload

---
+++ preprocess_caption.py
@@ -0,0 +1,42 @@
+
+from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
+import torch
+
+# Load model and tokenizer
+print("Loading the model")
+model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+max_length = 16
+num_beams = 4
+gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+
+# Function to generate caption
+def generate_caption(image_path):
+    image = Image.open(image_path)
+    image = image.convert("RGB")
+    pixel_values = feature_extractor(images=[image], return_tensors="pt").pixel_values.to(device)
+    output_ids = model.generate(pixel_values, **gen_kwargs)
+    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+    caption = preds[0].strip()
+    return caption
+
+def generate_caption(path):
+    images = []
+    for image_path in path:
+        i_image = Image.open(image_path)
+        if i_image.mode != "RGB":
+            i_image = i_image.convert(mode="RGB")
+        images.append(i_image)
+    
+    pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
+    pixel_values = pixel_values.to(device)
+    output_ids = model.generate(pixel_values, **gen_kwargs)
+
+    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+    preds = [pred.strip() for pred in preds]
+    print("Final Caption is:", preds)
+    return preds
+
