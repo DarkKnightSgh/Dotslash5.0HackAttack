f9527a18121f6650f49decd1d581639f0b7535c0
Siri Gowri H <106529095+DarkKnightSgh@users.noreply.github.com>
2024-03-24 00:18:02 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,19 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from preprocess_caption import predict_caption
 from flask_cors import CORS
 import os
-import webbrowser
+from gtts import gTTS
+from pydub import AudioSegment
+from pydub.playback import play
 
 # Create Flask application instance
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "*"}})  # Corrected the syntax here
-
-# Define the path to the templates directory
-template_dir = os.path.abspath('../templates')
-app = Flask(__name__, template_folder=template_dir)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 # Define routes
 @app.route('/')
-# def index():
-#     return render_template('mock.html')
+def index():
+    return "Hello, World!"
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -42,12 +40,22 @@ def upload_file():
         os.remove(image_path)
 
         print("Generated Caption:", caption)
+
+        # Convert caption to audio
+        caption = "".join(caption)
+        tts = gTTS(text=caption, lang='en')
+        tts_file = 'output.mp3'
+        tts.save(tts_file)
+        print("Audio file generated successfully.")
+
+        # Play the audio
+        audio = AudioSegment.from_mp3(tts_file)
+        play(audio)
+        
         return jsonify({'caption': caption}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 # Run the Flask application
 if __name__ == '__main__':
-    # Open the default web browser and navigate to the appropriate URL
-    # webbrowser.open('http://127.0.0.1:5000/')
     app.run(debug=True)
