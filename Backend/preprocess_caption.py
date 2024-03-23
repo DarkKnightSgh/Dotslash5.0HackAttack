38c068a69f630259df21fec1d8e3177d9915d8e4
Siri Gowri H <106529095+DarkKnightSgh@users.noreply.github.com>
2024-03-23 18:19:15 +0530

    Add files via upload

---
+++ Backend/preprocess_caption.py
@@ -0,0 +1,42 @@
+from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
+import torch
+from PIL import Image
+import tkinter as tk
+from tkinter import filedialog
+
+model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+max_length = 16
+num_beams = 4
+gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+
+def predict_caption(image_path):
+    i_image = Image.open(image_path)
+    if i_image.mode != "RGB":
+        i_image = i_image.convert(mode="RGB")
+    
+    pixel_values = feature_extractor(images=i_image, return_tensors="pt").pixel_values
+    pixel_values = pixel_values.to(device)
+    output_ids = model.generate(pixel_values, **gen_kwargs)
+
+    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+    preds = [pred.strip() for pred in preds]
+    print("Final Caption is:", preds)
+    return preds
+
+def select_image():
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
+    file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.jpg *.jpeg *.png")])
+    if file_path:
+        caption = predict_caption(file_path)
+        return caption
+    else:
+        return None
+
+caption = select_image()
